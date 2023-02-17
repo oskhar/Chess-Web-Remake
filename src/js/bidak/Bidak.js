fc1eda4fb2e5865133f3072d699a4da9d42ea222
muhamadoskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-02-17 16:51:05 +0700

    Menjadikan ClassBidak sebagai parent

--- src/js/bidak/Bidak.js
+++ src/js/bidak/Bidak.js
@@ -1,1 +1,21 @@
 import { ClassBidak } from "./class/ClassBidak.js";
+
+// Class
+class Pion extends ClassBidak {
+
+    // Constructor
+    constructor (ukuranArea, x, y, background) {
+
+        super(ukuranArea, x, y, background);
+        this.papanPermukaan = document.getElementById('Papan_Catur_Permukaan');
+
+    }
+
+    // Method
+    areaJalan () {
+
+
+
+    }
+
+} export {Pion};
--- src/js/bidak/class/ClassBidak.js
+++ src/js/bidak/class/ClassBidak.js
@@ -4,14 +4,38 @@ class ClassBidak {
     // Constructor
     constructor (ukuranArea, x, y, background) {
 
+        // Atribute
+        this.x = x;
+        this.y = y;
+        this.ukuranArea = ukuranArea;
+
+        // Create element
         this.element = document.createElement('div');
         this.element.style.position = "absolute";
-        this.element.style.width = ukuranArea+"px";
-        this.element.style.height = ukuranArea+"px";
-        this.element.style.left = x+"px";
-        this.element.style.top = y+"px";
+        this.element.style.width = this.ukuranArea+"px";
+        this.element.style.height = this.ukuranArea+"px";
+        this.element.style.left = this.x+"px";
+        this.element.style.top = this.y+"px";
         this.element.style.background = "url('" + background + "')";
         document.getElementById('Papan_Catur').appendChild(this.element);
+
+    }
+
+    // Method
+    move (langkahKedepan, langkahKesamping) {
+
+        this.y = (-langkahKedepan * this.ukuranArea + this.y);
+        this.element.style.top = this.y + "px";
+
+        this.x = (langkahKesamping * this.ukuranArea + this.x);
+        this.element.style.left = this.x + "px";
+
+    }
+
+    // Method
+    death () {
+
+        this.element.remove();
 
     }
 
